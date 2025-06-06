f849d4530744616cbb1a86e437d7e98d08f5ac0c
pablofbc <pablofbc@gmail.com>
2025-06-06 16:51:02 -0300

    new def 5

--- simple_utils.py
+++ simple_utils.py
@@ -29,3 +29,11 @@ def es_par(numero):
 def imprimir_elementos(lista):
     for i in range(len(lista)):  
         print(lista[i + 1])
+
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    if n == 0:
+        return 1
+    else:
+         return n * factorial(n - 1)
